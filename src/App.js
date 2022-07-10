5b1e359eb7dbb6f05fc43bcd6d2660a18d691a80
S-MunSoo <godmsoo93@gmail.com>
2022-07-10 20:21:38 +0900

    simpleDiary frist

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import DiaryEditor from "./component/DiaryEditor";
+
+// 1. 작성자 칸
+// 2. 일기본문
+// 3. 감정점수
+function App() {
+  return (
+    <div>
+      <DiaryEditor />
+    </div>
+  );
+}
+
+export default App;
